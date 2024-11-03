5f41e1ccff6ed3c14c5a3609e9341760afe467dc
aryno <arynovalmaz7@gmail.com>
2024-11-03 14:38:10 +0100

    final

---
+++ test/src/main/java/com/example/test/controllers/FirstController.java
@@ -0,0 +1,23 @@
+package com.example.test.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+
+    @GetMapping("/hello")
+        public String helloPage() {
+//        return "first/hello";
+        return "/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodByePage() {
+//        return "first/goodbye";
+        return "/goodbye";
+    }
+
+}
